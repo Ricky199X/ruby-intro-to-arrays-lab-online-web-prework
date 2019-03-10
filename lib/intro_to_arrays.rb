79cc3d263f1310a96fa758d475524fe8060c5bca
Ricky Rojas <rickyrojas19@gmail.com>
2019-03-10 23:06:58 +0000

    Automatically backed up by Learn

--- lib/intro_to_arrays.rb
+++ lib/intro_to_arrays.rb
@@ -5,3 +5,11 @@ end
 def array_with_two_elements
   my_two_array = ["Hello", "World"]
 end
+
+def first_element(array)
+  my_first_element = array.first
+end
+
+def third_element(array)
+  my_third_element = array[2]
+end
